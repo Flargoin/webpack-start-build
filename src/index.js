86d0664f339dfa1f188918de6a401c31b15086ce
Sergey <flargoin@gmail.com>
2024-08-06 22:08:47 +0300

    Базовая сборка Webpack

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import './template.html';
+import './index.scss';
+/* import code from './images/darkside1.png'; */
+import { mult, sum } from './modules/calc';
+
+/* const img = new Image();
+img.src = code;
+ */
+console.log(mult(2, 4));
+console.log(sum(2, 4));
